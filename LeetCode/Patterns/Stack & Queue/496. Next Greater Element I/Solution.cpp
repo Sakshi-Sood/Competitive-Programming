28fe05700bf7ae055c18eedea67e230609a010e5
Sakshi Sood <149409261+Sakshi-Sood@users.noreply.github.com>
2026-08-01 17:13:42 +0530

    Add C++ Solution to Patterns/Stack & Queue

--- LeetCode/Patterns/Stack & Queue/496. Next Greater Element I/Solution.cpp
+++ LeetCode/Patterns/Stack & Queue/496. Next Greater Element I/Solution.cpp
@@ -3,22 +3,24 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
+
         unordered_map<int, int> map;
+        vector<int> res;
         stack<int> st;
         
-        for(int i = m-1; i >= 0; i--) {
-            while(!st.empty() && st.top() <= nums2[i])
+        for(int num : nums2) {
+            while(!st.empty() && num > st.top()) {
+                map[st.top()] = num;
                 st.pop();
-            if(!st.empty())
-                map[nums2[i]] = st.top();
-            else
-                map[nums2[i]] = -1;
-            st.push(nums2[i]);
+            }
+            st.push(num);
         }
 
-        vector<int> res;
         for(int num : nums1) {
-            res.push_back(map[num]);
+            if(map.count(num))
+                res.push_back(map[num]);
+            else 
+                res.push_back(-1);
         }
         return res;
     }
