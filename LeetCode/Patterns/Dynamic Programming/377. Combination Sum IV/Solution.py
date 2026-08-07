45c5b9899b229e9e99731eb716b6c09ed3139c7d
Sakshi Sood <149409261+Sakshi-Sood@users.noreply.github.com>
2026-08-07 23:13:33 +0530

    Add Python Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/377. Combination Sum IV/Solution.py
+++ LeetCode/Patterns/Dynamic Programming/377. Combination Sum IV/Solution.py
@@ -1,19 +1,21 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        mem = [[-1] * (target+1) for _ in range(n)]
-        def memo(i: int, target: int)->int:
+        mem = [-1] * (target+1) 
+        def memo(target: int)->int:
             if target == 0:
                 return 1
-            if target < 0 or i >= n:
+            if target < 0:
                 return 0
-            if mem[i][target] != -1:
-                return mem[i][target]
+            if mem[target] != -1:
+                return mem[target]
 
-            pick = memo(0, target-nums[i])
-            notpick = memo(i+1, target)
-            mem[i][target] = pick + notpick
-            return mem[i][target]
+            ways = 0
+            for num in nums:
+                ways += memo(target - num)
 
-        return memo(0, target)
+            mem[target] = ways
+            return mem[target]
+
+        return memo(target)
         
