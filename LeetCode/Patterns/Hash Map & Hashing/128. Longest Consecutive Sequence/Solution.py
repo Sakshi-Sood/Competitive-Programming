a47b88da58b121fd592d90786a0e1647c3e0ca08
Sakshi Sood <149409261+Sakshi-Sood@users.noreply.github.com>
2026-08-08 00:06:54 +0530

    Add Python Solution to Patterns/Hash Map & Hashing

---
+++ LeetCode/Patterns/Hash Map & Hashing/128. Longest Consecutive Sequence/Solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        s = set(nums)
+        max_len = float('-inf')
+
+        for num in s:
+            if num-1 not in s:
+                l = 1
+                while num+l in s:
+                    l += 1
+                max_len = max(max_len, l)
+
+        return max_len
+
