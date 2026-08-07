5c8f64935d86672be8b85b785cb53d1308d898a8
Sakshi Sood <149409261+Sakshi-Sood@users.noreply.github.com>
2026-08-07 23:39:46 +0530

    Add Medium C++ Solution from LeetCode

---
+++ LeetCode/C++/Medium/128. Longest Consecutive Sequence/Solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) return 0;
+
+        int longest = 1;
+        unordered_set<int> st;
+
+        for(int num : nums)
+            st.insert(num);
+        
+        for(int num : st) {
+            if(st.find(num-1) == st.end()) {
+                int count = 1;
+                int x = num;
+                while(st.find(x+1) != st.end()) {
+                    x++;
+                    count++;
+                }
+                longest = max(longest, count);
+            }
+        }
+        return longest;
+    }
+};
