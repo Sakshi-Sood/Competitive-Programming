ab3acab93ce3cd2076c073293bd7c1fba26e0c16
Sakshi Sood <149409261+Sakshi-Sood@users.noreply.github.com>
2026-08-08 01:02:26 +0530

    Add Medium Python Solution from LeetCode

--- LeetCode/Python/Medium/377. Combination Sum IV/Solution.py
+++ LeetCode/Python/Medium/377. Combination Sum IV/Solution.py
@@ -1,21 +1,12 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        mem = [-1] * (target+1) 
-        def memo(target: int)->int:
-            if target == 0:
-                return 1
-            if target < 0:
-                return 0
-            if mem[target] != -1:
-                return mem[target]
+        dp = [0] * (target+1) 
+        dp[0] = 1
 
-            ways = 0
+        for t in range(1, target+1):
             for num in nums:
-                ways += memo(target - num)
+                if t-num >= 0:
+                    dp[t] += dp[t-num]
 
-            mem[target] = ways
-            return mem[target]
-
-        return memo(target)
-        
+        return dp[target]
