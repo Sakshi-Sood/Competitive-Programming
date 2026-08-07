fcf9f371e265f858fcbb12ec75ce776dacef61b7
Sakshi Sood <149409261+Sakshi-Sood@users.noreply.github.com>
2026-08-07 14:25:58 +0530

    Add Python Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/5. Longest Palindromic Substring/Solution.py
+++ LeetCode/Patterns/Dynamic Programming/5. Longest Palindromic Substring/Solution.py
@@ -8,7 +8,7 @@ class Solution:
         for i in range(n-1, -1, -1):
             for j in range(i, n):
                 if s[i] == s[j]:
-                    if j-i <= 2 or dp[i+1][j-1]:
+                    if j-i <= 1 or dp[i+1][j-1]:
                         dp[i][j] = True
                 
                 if dp[i][j] and j-i+1 > max_len:
