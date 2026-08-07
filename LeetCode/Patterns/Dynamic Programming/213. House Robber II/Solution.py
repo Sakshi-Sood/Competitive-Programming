5731796509265fe4fe5cb734c19dafbd5c3e8fab
Sakshi Sood <149409261+Sakshi-Sood@users.noreply.github.com>
2026-08-07 17:56:33 +0530

    Add Python Solution to Patterns/Dynamic Programming

---
+++ LeetCode/Patterns/Dynamic Programming/213. House Robber II/Solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        mem1 = [-1] * (n)
+        mem2 = [-1] * (n)
+
+        def memo(i: int, m: int, dp: List[int]) -> int:
+            if i > m:
+                return 0
+            if dp[i] != -1:
+                return dp[i]
+
+            dp[i] = max(nums[i] + memo(i+2, m, dp), memo(i+1, m, dp))
+            return dp[i]
+
+        return max(memo(0, n-2, mem1), memo(1, n-1, mem2))
