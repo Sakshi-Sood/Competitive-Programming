851bf06dac1d081f4db67a1a552ef396f33e59e1
Sakshi Sood <149409261+Sakshi-Sood@users.noreply.github.com>
2026-08-07 22:01:43 +0530

    Add C++ Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/377. Combination Sum IV/Solution.cpp
+++ LeetCode/Patterns/Dynamic Programming/377. Combination Sum IV/Solution.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
-    int combinationSum4(vector<int>& nums, int target) {
+    int solve(vector<int>& nums, vector<int>& dp, int target) {
         if(target == 0) return 1;
         if(target < 0) return 0;
+        
+        if(dp[target] != -1) return dp[target];
 
         int count = 0;
         for(int num : nums) 
-            count += combinationSum4(nums, target- num);
+            count += solve(nums, dp, target- num);
         
-        return count;
+        return dp[target] = count;
     }
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1, -1);
+        return solve(nums, dp, target);
+    }
+
+    
 };
