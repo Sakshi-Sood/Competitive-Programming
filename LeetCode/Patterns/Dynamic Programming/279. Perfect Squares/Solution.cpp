99de0dd29842bf7eb5df1f4790a5370df4248120
Sakshi Sood <149409261+Sakshi-Sood@users.noreply.github.com>
2026-08-08 01:33:11 +0530

    Add C++ Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/279. Perfect Squares/Solution.cpp
+++ LeetCode/Patterns/Dynamic Programming/279. Perfect Squares/Solution.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int numSquares(int n) {
         vector<int> dp(n+1, INT_MAX);
-        dp[0] = 0, dp[1] = 1;
+        dp[0] = 0;
 
-        for(int i = 2; i <= n; i++) 
-            for(int j = 1; j*j <= i; j++) 
-                dp[i] = min(dp[i-j*j] + 1, dp[i]);
-            
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j*j <= i; j++) {
+                dp[i] = min(dp[i], 1 + dp[i-(j*j)]);
+            }
+        }
         return dp[n];
     }
 };
