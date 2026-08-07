2cfe91451ef6de820bc8fc1b3c63c2a04b7f91e1
Sakshi Sood <149409261+Sakshi-Sood@users.noreply.github.com>
2026-08-07 22:20:14 +0530

    Add C++ Solution to Patterns/Backtracking

--- LeetCode/Patterns/Backtracking/39. Combination Sum/Solution.cpp
+++ LeetCode/Patterns/Backtracking/39. Combination Sum/Solution.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    void combination(vector<int>& candidates, int target, int i, vector<int>& curr, vector<vector<int>>& res) {
-        if(target == 0) {
+
+    void makeCombination(vector<int>& arr, vector<int>& curr, vector<vector<int>>& res, int remSum, int index) {
+        if(remSum == 0) {
             res.push_back(curr);
             return;
         }
-        if(target < 0 || i >= candidates.size())
+        if(remSum < 0 || index >= arr.size()) 
             return;
 
-        curr.push_back(candidates[i]);
-        combination(candidates, target - candidates[i], i, curr, res);
+        curr.push_back(arr[index]);
+        makeCombination(arr, curr, res, remSum-arr[index], index);
         curr.pop_back();
-        combination(candidates, target, i+1, curr, res);
+        makeCombination(arr, curr, res, remSum, index+1);
+        
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
         vector<int> curr;
-        int i = 0;
-        combination(candidates, target, i, curr, res);
+        vector<vector<int>> res;
+
+        makeCombination(candidates, curr, res, target, 0);
         return res;
     }
 };
