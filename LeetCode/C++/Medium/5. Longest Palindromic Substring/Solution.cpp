acdc76346dd22ec77c29f4dd5f1a5de8d9fc99ba
Sakshi Sood <149409261+Sakshi-Sood@users.noreply.github.com>
2026-08-08 00:31:57 +0530

    Add Medium C++ Solution from LeetCode

--- LeetCode/C++/Medium/5. Longest Palindromic Substring/Solution.cpp
+++ LeetCode/C++/Medium/5. Longest Palindromic Substring/Solution.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    bool solve(int i, int j, string &s) {
-        if(i >= j) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s[i] == s[j])
-            return dp[i][j] = solve(i+1, j-1, s);
-        return dp[i][j] = 0;
-    }
-    
     string longestPalindrome(string s) {
         int n = s.length();
-        memset(dp, -1, sizeof(dp));
+        vector<vector<bool>> dp(n, vector<bool> (n, false));
         int maxLen = INT_MIN;
         int sp = 0;
 
-        for(int i = 0; i < n; i++) 
-            for(int j = i; j < n; j++) 
-                if(solve(i, j, s)) {
-                    if(j-i+1 > maxLen){
-                        maxLen = max(maxLen, j-i+1);
-                        sp = i;
-                    }
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = i; j < n; j++) {
+                if(s[i] == s[j]) {
+                    if(j-i <= 2 || dp[i+1][j-1])
+                        dp[i][j] = true;
                 }
+                if(dp[i][j] && j-i+1 > maxLen) {
+                    maxLen = j-i+1;
+                    sp = i;
+                }
+            }
+        }
         return s.substr(sp, maxLen);
     }
 };
