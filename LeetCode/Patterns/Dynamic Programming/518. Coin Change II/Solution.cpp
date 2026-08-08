d3ec4e586ef4ba8ae863a7a269d483625bade0a5
Sakshi Sood <149409261+Sakshi-Sood@users.noreply.github.com>
2026-08-08 14:23:20 +0530

    Add C++ Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/518. Coin Change II/Solution.cpp
+++ LeetCode/Patterns/Dynamic Programming/518. Coin Change II/Solution.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int solve(int i, int target, vector<int> &coins) {
+    int solve(int i, int target, vector<int> &coins, vector<vector<int>> &dp) {
         if(target == 0)
             return 1;
         if(target < 0 || i >= coins.size())
             return 0;
         
-        int pick = solve(i, target-coins[i], coins);
-        int skip = solve(i+1, target, coins);
+        if(dp[i][target] != -1)
+            return dp[i][target];
+        
+        int pick = solve(i, target-coins[i], coins, dp);
+        int skip = solve(i+1, target, coins, dp);
 
-        return pick + skip;
+        return dp[i][target] = pick + skip;
     }
     int change(int amount, vector<int>& coins) {
-        
-        return solve(0, amount, coins);
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int> (amount+1, -1));
+        return solve(0, amount, coins, dp);
     }
 };
